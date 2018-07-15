85645e90cfda79a9aafaeda667c4d69f33105d8f
EmilSchroder <eschro66@gmail.com>
2018-07-16 10:01:49 +1200

    updated adws controls to trigger

--- animate.js
+++ animate.js
@@ -6,6 +6,44 @@ var x = canvas.width/2;
 var y = canvas.height/2;
 var ballrad = 10;
 
+//Starting postions for directional buttons
+var leftPressed = false;
+var rightPressed = false;
+var upPressed = false;
+var downPressed = false;
+
+//Listen for button pushes
+document.addEventListener("keydown",keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+//what happens when button pushed
+function keyDownHandler(evt){
+    if (evt.keyCode === 65){
+        leftPressed = true;
+    } else if (evt.keyCode===68){
+        rightPressed = true;
+    } else if (evt.keyCode===87){
+        upPressed = true;
+    } else if (evt.keyCode===83){
+        downPressed = true;
+    }
+}
+
+//what happen when button release
+function keyDownHandler(evt){
+    if (evt.keyCode === 65){
+        leftPressed = false;
+    } else if (evt.keyCode===68){
+        rightPressed = false;
+    } else if (evt.keyCode===87){
+        upPressed = false;
+    } else if (evt.keyCode===83){
+        downPressed = false;
+    }
+}
+
+
+
 //Draw the ball
 function drawBall(){
     ctx.beginPath();
@@ -15,6 +53,11 @@ function drawBall(){
     ctx.closePath();
 }
 
+//move the ball
+function moveBall(){
+    
+}
+
 
 
 //Function to kick it off
@@ -22,6 +65,7 @@ function draw(){
     ctx.clearRect(0,0,canvas.width,canvas.height);
 
     drawBall();
+    moveBall();
 }
 
 draw();
